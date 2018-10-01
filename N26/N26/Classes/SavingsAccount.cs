096c7e1ff720565f6890a0a7cba3b4e2ada9354d
Torben Lehmann <torben.leh@outlook.com>
2018-10-01 13:37:51 +0200

    Some SavingsAccount progress (Can't finish since I don't have a savings account atm)

---
+++ N26/N26/Classes/SavingsAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N26.Classes
+{
+    class SavingsAccount
+    {
+        public string id;
+        public string name;
+        public double monthlyAmount;
+        public List<SavingsHistory> history;
+    }
+}
---
+++ N26/N26/Classes/SavingsHistory.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N26.Classes
+{
+    class SavingsHistory
+    {
+        public string name;
+        public string date;
+        public double value;
+        public double profit;
+        public double profitPercentage;
+
+        public SavingsHistory(JObject jObject)
+        {
+            name = jObject.GetValue("name").ToString();
+            date = jObject.GetValue("date").ToString();
+            value = (double) jObject.GetValue("value");
+            profit = (double)jObject.GetValue("profit");
+            profitPercentage = (double)jObject.GetValue("profitPercentage");
+        }
+    }
+}
